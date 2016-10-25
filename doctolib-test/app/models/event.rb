d26efd595639bf793c1ebf55d4f04e418456847d
Deuteu <Deuteu@users.noreply.github.com>
2016-10-25 23:46:55 +0200

    Add day availabilities.

--- doctolib-test/app/models/event.rb
+++ doctolib-test/app/models/event.rb
@@ -4,7 +4,47 @@ class Event < ActiveRecord::Base
   validate :halfHour
   #Enum could be use but need to change :kind type in database
 
-private
+  HOUR_FORMAT = '%-k:%M'.freeze
+  SLOT_DURATION = 30.minutes
+
+  def self.dayAvailabilities(date = DateTime.now)
+    openings = dayAvailabilitiesKind(date, "opening")
+    appointments = dayAvailabilitiesKind(date, "appointment")
+    (openings - appointments).sort_by(&:to_i)
+  end
+
+  private
+    def self.dayAvailabilitiesKind(date ,kind)
+      availabilities = []
+      #Availabilities from non recurring events
+      daylyEvents = Event.where("starts_at < ?", date.end_of_day.utc).where("? < ends_at", date.beginning_of_day.utc).where(weekly_recurring: nil, kind: kind)
+      for event in daylyEvents
+        #Take beginning of the event or the beginning of the day if starts the day before
+        slot = [event.starts_at, date.beginning_of_day].max
+        #Stop at the end of the event or the end of the day if ends the day after
+        while slot < [event.ends_at, date.end_of_day].min
+          availabilities.push(slot.strftime(HOUR_FORMAT))
+          slot = (slot + SLOT_DURATION)
+        end
+      end
+      #Availabilities from recurring event (only past ones)
+      weeklyEvents = Event.where("starts_at < ?", date.end_of_day.utc).where(weekly_recurring: true, kind: kind)
+      for event in weeklyEvents
+        #Containing this day of week
+        sameWeek = event.starts_at.wday <= event.ends_at.wday && event.starts_at.wday <= date.wday && date.wday <= event.ends_at.wday
+        overTwoWeek = event.starts_at.wday > event.ends_at.wday && (event.starts_at.wday >= date.wday || date.wday >= event.ends_at.wday)
+        if (sameWeek || overTwoWeek)
+          #Take beginning of the eveny or the beginning of the day if starts the day before
+          slot = (event.starts_at.wday == date.wday) ? event.starts_at : date.beginning_of_day
+          limit = (event.ends_at.wday == date.wday) ? event.ends_at : date.end_of_day
+          while slot < limit
+            availabilities.push(slot.strftime(HOUR_FORMAT))
+            slot = (slot + SLOT_DURATION)
+          end
+        end
+      end
+      availabilities
+    end
 
     def halfHour
       if self.starts_at != roundDown(self.starts_at)
